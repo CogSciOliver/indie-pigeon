300f8cc589bbf5d96e32d7c39ee15b7d2b38c3f4
chiva-emdr <danii@chivaemdr.com>
2026-03-09 22:59:56 -0500

    complete flow reset .dbv0.1.1_26.03.09-22:41

--- app/db.py
+++ app/db.py
@@ -1,9 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from .models import Base
 
-engine = create_engine("sqlite:///./app.db", connect_args={"check_same_thread": False})
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
